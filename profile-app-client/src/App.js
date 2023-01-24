542ae1926f52dd576b4f1811cd465ba5d8022866
Iryna1791 <irynawebdev@gmail.com>
2023-01-24 18:31:43 +0100

    done

---
+++ profile-app-client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+     <Route path='/' element={<HomePage />} />
+     <Route path='/signup' element={<SignUp />} />
+     <Route path='/login' element={<LogIn />} />
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
